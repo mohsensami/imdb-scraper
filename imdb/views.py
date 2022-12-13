6e026199e450794cbec5a39548f324e47005e6f8
MohsenSami <mohsensami88@gmail.com>
2022-12-13 19:58:16 +0330

    requests and bs4 packages added and show top250 imdb items in home page

--- imdb/views.py
+++ imdb/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+import requests
+from bs4 import BeautifulSoup
+
 
 def top250Movies(request):
-    return render(request, 'base.html')
+    url = 'http://www.imdb.com/chart/top'
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    items = soup.find_all('td', {'class':'posterColumn'})
+    context = {'result': items}
+    return render(request, 'base.html', context)
