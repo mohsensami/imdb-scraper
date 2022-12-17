95e9f36c8a1400adcabadc838f319caadc592850
MohsenSami <mohsensami88@gmail.com>
2022-12-17 17:44:24 +0330

    get boxoffice wekkend table in one

--- imdb/views.py
+++ imdb/views.py
@@ -10,27 +10,28 @@ sys.setrecursionlimit(7500)
 
 
 def boxOfficeMovies(request):
-    url = 'http://www.imdb.com/chart/boxoffice'
+    url = 'https://imdb-api.com/box-office'
     page = requests.get(url)
     soup = BeautifulSoup(page.text, 'html.parser')
 
-    table = soup.find('tbody')
-    rows = table.find_all('tr')
-    data = []
-    for row in rows:
-        obj, created = Movie.objects.get_or_create(
-            image = row.find('td', {'class':'posterColumn'}).find('img')['src'].replace('\n', ''),
-            title = row.find('td', {'class':'titleColumn'}).text.replace('\n', ''),
-            slug = slugify(row.find('td', {'class':'titleColumn'}).text.replace('\n', '')),
-            weekend = row.find('td', {'class':'ratingColumn'}).text.replace('\n', '').strip(),
-            gross = row.find('td', {'class':'ratingColumn'}).text.replace('\n', '').strip(),
-            weeks = row.find('td', {'class':'weeksColumn'}).text.replace('\n', ''),
-            category='b'
-        )
-        data.append(obj)
+    table = soup.find('table')
+    # rows = table.find_all('tr')
+    data = ''
+    # for row in rows:
+    #     obj, created = Movie.objects.get_or_create(
+    #         image = row.find('td', {'class':'posterColumn'}).find('img')['src'].replace('\n', ''),
+    #         title = row.find('td', {'class':'titleColumn'}).text.replace('\n', ''),
+    #         slug = slugify(row.find('td', {'class':'titleColumn'}).text.replace('\n', '')),
+    #         weekend = row.find('td', {'class':'ratingColumn'}).text.replace('\n', '').strip(),
+    #         gross = row.find('td', {'class':'ratingColumn'}).text.replace('\n', '').strip(),
+    #         weeks = row.find('td', {'class':'weeksColumn'}).text.replace('\n', ''),
+    #         category='b'
+    #     )
+    #     data.append(obj)
+    data += str(table)
 
     context = {'result': data}
-    return render(request, 'rows.html', context)
+    return render(request, 'table.html', context)
 
 
 def top250Movies(request):
